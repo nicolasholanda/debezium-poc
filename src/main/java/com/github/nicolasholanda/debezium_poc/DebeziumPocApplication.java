672f2aee8985a35b85d2268c845c0a17606c1ff3
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-07-04 21:43:00 -0300

    Initial commit

---
+++ src/main/java/com/github/nicolasholanda/debezium_poc/DebeziumPocApplication.java
@@ -0,0 +1,13 @@
+package com.github.nicolasholanda.debezium_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebeziumPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebeziumPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nicolasholanda/debezium_poc/DebeziumPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nicolasholanda.debezium_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebeziumPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
